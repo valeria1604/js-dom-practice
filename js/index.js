f399d37736f99705cba7342215b1892607db5198
valeria1604 <117079225+valeria1604@users.noreply.github.com>
2024-08-31 15:30:33 +0000

    Carousel

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function init() {
     import('./index.header-text.js');
     import('./index.header-clock.js');
     import('./index.product-list.js');
-
+    import('./index.product-carousel.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
---
+++ js/index.product-carousel.js
@@ -0,0 +1,39 @@
+const slides = [
+    '<div><img src="img/baby-yoda.svg" alt="Baby Yoda"></div>',
+    '<div><img src="img/banana.svg" alt="Banana"></div>',
+    '<div><img src="img/girl.svg" alt="Girl"></div>',
+    '<div><img src="img/viking.svg" alt="Viking"></div>',
+];
+
+let currentSlideIdx = 0;
+
+function renderSlide() {
+    const slideContainer = document.querySelector('.product-crousel__slides');
+    slideContainer = slides[currentSlideIdx]
+    if(window.matchMedia('(min-width:768px)').matches) {
+        const secondSlideIdx = currentSlideIdx + 1 >= slidex.length ? 0 : currentSlideIdx + 1;
+        if(window.matchMedia('(min-width:1024px)').matches){
+            const thirdSlideIndx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIndx];
+        }
+    }
+}
+
+function nextSlide() {
+    currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+    renderSlide();
+}
+
+function preSlide() {
+    currentSlideIdx = currentSlideIdx - 1 <= 0 ? slides.length - 1 : currentSlideIdx - 1;
+    renderSlide();
+}
+// setInterval(nextSlide, 3000);
+
+renderSlide();
+
+const btnNext = document.querySelector('.product-carousel__btn-next');
+btnNext.addEventListener('click', nextSlide);
+
+const btnPrev = document.querySelector('.product-carousel__btn-next');
+btnNext.addEventListener('click', preSlide);
